6303f9103881c5835de63f0c48bd7aced347ed86
Jason Gross <jgross@mit.edu>
2026-02-06 19:29:46 -0800

    Add some tests from https://github.com/rocq-community/coq-dpdgraph/pull/173

---
+++ tests/PeanoNatBitwise.cmd
@@ -0,0 +1,4 @@
+Load "PeanoNatBitwise.v".
+Require Import dpdgraph.dpdgraph.
+Set DependGraph File "PeanoNatBitwise.dpd".
+Print DependGraph Stdlib.Arith.PeanoNat.Nat.PrivateImplementsBitwiseSpec.land_spec.
---
+++ tests/Proofs.cmd
@@ -0,0 +1,5 @@
+Load "Proofs.v".
+From dpdgraph Require Import dpdgraph.
+Set Printing Fully Qualified.
+Set DependGraph File "Proofs.dpd".
+Print DependGraph Stdlib.Arith.PeanoNat.Nat.PrivateImplementsBitwiseSpec.land_spec.
